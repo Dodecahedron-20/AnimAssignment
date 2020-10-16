d5498402efc778c67571d46ef5caf850a36975ac
Al <theonlypurplecat@gmail.com>
2020-10-16 15:33:30 +1300

    first

---
+++ Assets/Scripts/AzzizMovement.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AzzizMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    private float Speed;
+    [SerializeField]
+    private float JumpSpeed;
+
+
+    //All KeyCodes
+    [SerializeField]
+    private KeyCode Forward;
+    [SerializeField]
+    private KeyCode Backwards;
+    [SerializeField]
+    private KeyCode Jump;
+
+
+    //Animations parts
+
+    [SerializeField]
+    private Animator Anim;
+   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var horiz = 0;
+        var vert = 0;
+
+        if (Input.GetKey(Forward))
+        {
+            vert += 1;
+            Anim.SetBool("Walk", true);
+        }
+        else
+        {
+            Anim.SetBool("Walk", false);
+        }
+        if (Input.GetKey(Backwards))
+        {
+            vert -= 1;
+        }
+
+        var movement = new Vector3(horiz, 0, vert).normalized * Speed * Time.deltaTime;
+        transform.position += movement;
+
+
+        if (Input.GetKey(Jump))
+        {
+            Anim.SetBool("Jump", true);
+            
+        }
+        else
+        {
+            Anim.SetBool("Jump", false);
+        }
+
+
+    }
+
+   
+
+
+
+    
+
+
+
+
+}
---
+++ Assets/Scripts/KalMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KalMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    private float Speed;
+
+    [SerializeField]
+    private KeyCode SideAttack;
+    [SerializeField]
+    private KeyCode MoveRight;
+
+
+
+    //Animation things
+    [SerializeField]
+    private Animator Anim;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move Right things;
+
+        var horiz = 0;
+
+        if (Input.GetKeyDown(MoveRight))
+        {
+            horiz += 1;
+        }
+
+        var movement = new Vector3(horiz, 0, 0).normalized * Speed * Time.deltaTime;
+        transform.position += movement;
+
+
+
+
+        //Side attack things
+
+
+        if (Input.GetKeyDown(SideAttack))
+        {
+            Anim.SetBool("Attk1", true);
+        }
+        else
+        {
+            Anim.SetBool("Attk1", false);
+        }
+    }
+}
